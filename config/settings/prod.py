5d34f4bade353775e9b4ff6fbf5d8200a0b2c03d
Githaiga <pythongeeko@gmail.com>
2023-10-14 18:29:33 +0300

    comment cloudinary storage

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -144,45 +144,45 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-import logging
-import colorlog
+# import logging
+# import colorlog
 
-# Logging settings
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'colorful',
-        },
-        'file': {
-            'class': 'logging.FileHandler',
-            'level': 'DEBUG',
-            'filename': 'django.log',
-            'formatter': 'standard',
-        },
-    },
-    'formatters': {
-        'colorful': {
-            '()': colorlog.ColoredFormatter,
-            'format': '[%(asctime)s] %(log_color)s%(levelname)s:%(name)s:%(message)s',
-            'datefmt': '%d/%b/%Y %H:%M:%S', 
-        },
-        'standard': {
-            'format': '[%(asctime)s] %(levelname)s:%(name)s:%(message)s',
-            'datefmt': '%d/%b/%Y %H:%M:%S',
-        },
-    },
-    'loggers': {
-        '': {
-            'handlers': ['console', 'file'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-    },
-}
+# # Logging settings
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'level': 'DEBUG',
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'colorful',
+#         },
+#         'file': {
+#             'class': 'logging.FileHandler',
+#             'level': 'DEBUG',
+#             'filename': 'django.log',
+#             'formatter': 'standard',
+#         },
+#     },
+#     'formatters': {
+#         'colorful': {
+#             '()': colorlog.ColoredFormatter,
+#             'format': '[%(asctime)s] %(log_color)s%(levelname)s:%(name)s:%(message)s',
+#             'datefmt': '%d/%b/%Y %H:%M:%S', 
+#         },
+#         'standard': {
+#             'format': '[%(asctime)s] %(levelname)s:%(name)s:%(message)s',
+#             'datefmt': '%d/%b/%Y %H:%M:%S',
+#         },
+#     },
+#     'loggers': {
+#         '': {
+#             'handlers': ['console', 'file'],
+#             'level': 'INFO',
+#             'propagate': True,
+#         },
+#     },
+# }
 
 
 
