1bcec4621580a1860703f00d96209cc42878ca1c
Githaiga <pythongeeko@gmail.com>
2023-10-14 18:02:14 +0300

    prod-finally-final

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -253,5 +253,6 @@ CACHES = {
     }
 }
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
