c15ac97584dcb52af2572d3320b2646fc6fa0042
Githaiga <pythongeeko@gmail.com>
2024-02-10 23:06:47 +0300

    remove celery

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,5 @@ pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate
 
-celery -A config worker -l INFO
-celery -A config beat -l INFO
+# celery -A config worker -l INFO
+# celery -A config beat -l INFO
